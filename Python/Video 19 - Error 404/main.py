2ca0c7e6b99b1ac7e949f5fd414dc46f315814c9
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2025-10-01 18:29:25 -0600

    video 19 error 404

---
+++ Python/Video 19 - Error 404/main.py
@@ -0,0 +1,62 @@
+from ast import Pass
+from email import message
+from sre_constants import SUCCESS
+from urllib import response
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
+from flask import make_response
+from flask import flash
+
+from flask import url_for
+from flask import redirect
+
+from flask_wtf import CSRFProtect
+import forms
+
+app = Flask(__name__)
+app.secret_key = 'my_secret_key'
+csrf = CSRFProtect(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/') 
+def index():
+    if 'username' in session:
+        username = session['username']
+        print (username)
+    title = "Index"
+    return render_template('index.html', title = title)
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.pop('username')
+    return redirect(url_for('login'))
+    
+@app.route('/login', methods = ['GET','POST']) 
+def login():
+    login_form = forms.LoginForm(request.form)
+    if request.method == 'POST' and login_form.validate():
+        username = login_form.username.data
+        success_message = 'Bienvenido {}'.format(username)
+        flash(success_message)
+        
+        session['username'] = login_form.username.data
+    return render_template('login.html', form = login_form)
+
+@app.route('/cookie')
+def cookie():
+    response = make_response( render_template('cookie.html') )
+    response.set_cookie('custome_cookie ', 'Adrian')
+    return response
+
+@app.route('/comment', methods = ['GET','POST'])
+def comment():
+    return render_template('comment.html')
+    
+if __name__ == '__main__':
+    app.run( debug = True, port = 8000)
