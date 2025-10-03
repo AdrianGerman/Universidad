9fe629e22c2f20b0eba5fa83a3e7d8b420584b42
AdrianGerman <adrian.german1019@gmail.com>
2025-10-02 19:32:19 -0600

    video 24 modelo

---
+++ Python/Video 24 - Modelo/main.py
@@ -0,0 +1,89 @@
+from ast import Pass
+from email import message
+from sre_constants import SUCCESS
+from urllib import response
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
+from flask import make_response
+from flask import flash
+from flask import g
+from flask import url_for
+from flask import redirect
+
+from config import DevelopmentConfig
+
+from models import db
+from models import User
+
+
+from flask_wtf import CSRFProtect
+import forms
+import json
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf = CSRFProtect()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.before_request
+def before_request():
+    pass
+        
+@app.after_request
+def after_request(response):
+    return response
+
+@app.route('/') 
+def index():
+    if 'username' in session:
+        username = session['username']
+        print (username)
+    title = "Index"
+    return render_template('index.html', title = title)
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.pop('username')
+    return redirect(url_for('login'))
+    
+@app.route('/login', methods = ['GET','POST']) 
+def login():
+    login_form = forms.LoginForm(request.form)
+    if request.method == 'POST' and login_form.validate():
+        username = login_form.username.data
+        success_message = 'Bienvenido {}'.format(username)
+        flash(success_message)
+        session['username'] = login_form.username.data
+    return render_template('login.html', form = login_form)
+
+@app.route('/cookie')
+def cookie():
+    response = make_response( render_template('cookie.html') )
+    response.set_cookie('custome_cookie ', 'Adrian')
+    return response
+
+@app.route('/comment', methods = ['GET','POST'])
+def comment():
+    return render_template('comment.html')
+    
+@app.route('/ajax-login', methods = ['POST'])
+def ajax_login():
+    print (request.form)
+    username = request.form['username']
+    response = { 'status': 200, 'username': username, 'id': 1}
+    return json.dumps(response)
+
+if __name__ == '__main__':
+    csrf.init_app(app)
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
+        
+    app.run(port = 8000)
